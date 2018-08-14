271f49c58dbaaa310a7ef8137428633f0f632440
syhl94 <samyhlee94@gmail.com>
2018-08-13 18:39:30 -0700

    Refactors names of register view and activity.  Adds circular image view and firebase database/storage handling for users.

---
+++ app/src/main/java/com/example/sam/ezmessenger/RegisterActivity.kt
@@ -0,0 +1,127 @@
+package com.example.sam.ezmessenger
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener({
+            performRegister()
+        })
+
+        already_have_account_textview_register.setOnClickListener({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
+        photo_button_login.setOnClickListener({
+            Log.d("RegisterActivity", "Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        })
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // Check tos ee what the selected image is
+            Log.d("RegisterActivity", "Photo was selected successfully")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+//            val bitmapDrawable = BitmapDrawable(bitmap)
+//            photo_button_login.setBackgroundDrawable(bitmapDrawable)
+
+            photo_imageview_register.setImageBitmap(bitmap)
+            photo_button_login.alpha = 0f
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("RegisterActivity", "Email is: $email")
+        Log.d("RegisterActivity", "Password is: $password")
+
+        // Firebase Authentication
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    // Created user successfully
+                    Log.d("RegisterActivity", "Successfully created user with uid: ${it.result.user.uid}")
+
+                    // Upload image to Firebase
+                    uploadImageToFirebaseStorage()
+                }
+                .addOnFailureListener {
+                    Log.d("RegisterActivity", "Failed to create user: ${it.message}")
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if (selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+                .addOnSuccessListener {
+                    Log.d("RegisterActivity", "Successfully uploaded image: ${it.metadata?.path}")
+
+                    ref.downloadUrl.addOnSuccessListener {
+                        Log.d("RegisterActivity", "File Location: $it")
+
+                        saveUserToFirebaseDatabase(it.toString())
+                    }
+                }
+                .addOnFailureListener({
+                    Log.d("RegisterActivity", "Failed to load image: ${it.message}")
+                })
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user)
+                .addOnSuccessListener {
+                    Log.d("RegisterActivity", "Finally we saved the user to Firebase Database")
+                }
+                .addOnFailureListener({
+                    Log.d("RegisterActivity", "Failed to save user to Firebase Database: ${it.message}")
+                })
+    }
+}
---
+++ app/src/main/java/com/example/sam/ezmessenger/User.kt
@@ -0,0 +1,3 @@
+package com.example.sam.ezmessenger
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
